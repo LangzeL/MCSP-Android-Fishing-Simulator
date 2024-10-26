271abc6193474e9bbe933b41a512d97f487c07a9
shanqingh <shanqingh@student.unimelb.edu.au>
2024-10-26 23:02:40 +1100

    updated camera

--- Assets/Script/CameraBackground.cs
+++ Assets/Script/CameraBackground.cs
@@ -22,34 +22,15 @@ public class CameraBackground : MonoBehaviour
             // 将摄像头的画面设置为 RawImage 的纹理
             rawImage.texture = webcamTexture;
 
-            // 设置摄像头画面的旋转角度
+            // 旋转画面以适应竖屏显示
+            rawImage.rectTransform.localEulerAngles = new Vector3(0, 0, -90);
+
+            // 开始播放摄像头画面
             webcamTexture.Play();
-            rawImage.uvRect = GetRotatedUVRect(webcamTexture.videoRotationAngle);
-
-            // 设置 RawImage 的宽高比和摄像头画面匹配
-            rawImage.rectTransform.localScale = new Vector3(1, -1, 1); // 镜像翻转以获得正确方向
-
-            // 设置 RawImage 的尺寸
-            rawImage.rectTransform.sizeDelta = new Vector2(webcamTexture.width, webcamTexture.height);
         }
         else
         {
             Debug.LogError("没有检测到摄像头");
-        }
-    }
-
-    private Rect GetRotatedUVRect(int angle)
-    {
-        switch (angle)
-        {
-            case 90:
-                return new Rect(1, 0, -1, 1);
-            case 180:
-                return new Rect(1, 1, -1, -1);
-            case 270:
-                return new Rect(0, 1, 1, -1);
-            default:
-                return new Rect(0, 0, 1, 1);
         }
     }
 
