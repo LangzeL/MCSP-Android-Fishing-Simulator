bb2ff6712e6320529478fe10851315beefd592b3
shanqingh <shanqingh@student.unimelb.edu.au>
2024-10-28 00:59:38 +1100

    Revert "Merge pull request #14 from LangzeL/UI"
    This reverts commit 009d9e3ca48adbba4c0b855beda5f54111053e81, reversing
    changes made to 716c95d943906c481546b9797e01f0d5c8558799.

--- Assets/Script/Firebase/GameManager.cs
+++ Assets/Script/Firebase/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Firebase.Auth;
 using UnityEngine;
 
@@ -7,7 +6,6 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public UserData currentUserData;
-    public List<FishData> allFishData;
 
     void Awake()
     {
@@ -23,37 +21,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
-    // Load all fish data
-    public void LoadAllFishData()
-    {
-        // Manually add fish data or load from a resource file
-        // Example:
-        // allFishData = new List<FishData>();
-        // allFishData.Add(new FishData { fishID = "001", name = "Goldfish", score = 10, image = goldfishSprite });
-        // ...
-    }
-
     // Method to save the current user's data
     public void SaveCurrentUserData()
     {
         string userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
         DatabaseManager.Instance.SaveUserData(userId, currentUserData);
-    }
-
-    // Call this method when a player catches a fish
-    public void OnFishCaught(string fishID, int score)
-    {
-        // Update player's caught fish data
-        if (!currentUserData.fishCaught.Contains(fishID))
-        {
-            currentUserData.fishCaught.Add(fishID);
-        }
-
-        // Update player's total score
-        currentUserData.totalScore += score;
-
-        // Save user data
-        SaveCurrentUserData();
     }
 }
