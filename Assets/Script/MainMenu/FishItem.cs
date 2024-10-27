b840a906ef3b51b9cf83124555bc02fc20f68324
shanqingh <shanqingh@student.unimelb.edu.au>
2024-10-28 03:28:42 +1100

    added handbook, items and leaderboard

---
+++ Assets/Script/MainMenu/FishItem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishItem : MonoBehaviour
+{
+    public Image fishImage;
+    public Text fishNameText;
+    public GameObject lockOverlay; // Lock overlay UI element
+
+    private FishData fishData;
+
+    public void Setup(FishData data, bool isCaught)
+    {
+        fishData = data;
+        fishImage.sprite = data.image;
+
+        if (isCaught)
+        {
+            fishNameText.text = data.name;
+        }
+        else
+        {
+            fishNameText.text = "Unknown";
+        }
+
+        lockOverlay.SetActive(!isCaught);
+
+        // Optionally, add button event to view details
+    }
+}
+
